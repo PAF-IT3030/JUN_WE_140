ba9f03d8940fdf6fd2c96465fff32c3ad16e268d
chamuda-arangalla <chamudaarangalla@gmail.com>
2024-04-16 20:40:05 +0530

    Initial Commit front end and backend.

---
+++ backend/src/main/java/com/backend/strengthhub/StrengthhubApplication.java
@@ -0,0 +1,13 @@
+package com.backend.strengthhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrengthhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrengthhubApplication.class, args);
+	}
+
+}
