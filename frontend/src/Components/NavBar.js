60659b46fe8e9ee290c65803b7cb3f174c77b475
chamuda-arangalla <chamudaarangalla@gmail.com>
2024-04-19 17:16:14 +0530

    Create Navbar and install MUI .

---
+++ frontend/src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import {AppBar, IconButton, Toolbar, Typography} from '@mui/material'
+import logo from '../Images/logo.jpg'
+function NavBar() {
+  return (
+    <AppBar position='static'>
+        <Toolbar>
+            <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
+                <logo/>
+            </IconButton>
+        </Toolbar>
+        <Typography variant='h6' component='div'> StrengthHub </Typography>
+    </AppBar>
+  )
+}
+
+export default NavBar
---
+++ frontend/src/Pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../Components/NavBar'
+import PrimarySearchAppBar from '../Components/PrimarySearchAppBar'
+
+function HomePage() {
+  return (
+    <div>
+      {/* <NavBar/> */}
+      <PrimarySearchAppBar/>
+      <h2>HomePage</h2>
+    </div>
+  )
+}
+
+export default HomePage
