837468686d7b7dd5445744307174a37b588440df
chamuda-arangalla <chamudaarangalla@gmail.com>
2024-04-19 21:31:58 +0530

    Create sideBar and post.

--- frontend/src/Components/Post.js
+++ frontend/src/Components/Post.js
@@ -1,4 +1,10 @@
-import { Favorite, FavoriteBorder, MoreVert, Share } from "@mui/icons-material";
+import {
+  Description,
+  Favorite,
+  FavoriteBorder,
+  MoreVert,
+  Share,
+} from "@mui/icons-material";
 import {
   Avatar,
   Card,
@@ -10,7 +16,21 @@ import {
   IconButton,
   Typography,
 } from "@mui/material";
+import workoutImg from "../Images/workout.jpg";
+import { useState } from "react";
+
 const Post = () => {
+  const IncommingPost = {
+    name: "Jhon",
+    date: "September 14, 2022",
+    description:
+      "Hey everyone! 💪 Just crushed a killer full-body workout! Started with a 10-minute dynamic warm-up to get the blood flowing, then hit three rounds of circuits:Squats, push-ups, and bent-over rows for strength.Jumping jacks, mountain climbers, and burpees for cardio.Plank holds, Russian twists, and bicycle crunches for core work.Finished off with a 5-minute cool-down and stretch. Feeling the burn in all the right places! Who's joining me next time? #WorkoutMotivation #FitnessGoals",
+    image: workoutImg,
+  };
+
+  const [post, setPost] = useState(IncommingPost);
+
+  console.log(post, "values");
   return (
     <Card sx={{ margin: 5 }}>
       <CardHeader
@@ -24,20 +44,18 @@ const Post = () => {
             <MoreVert />
           </IconButton>
         }
-        title="John Doe"
-        subheader="September 14, 2022"
+        title={post.name}
+        subheader={post.date}
       />
       <CardMedia
         component="img"
         height="20%"
-        image="https://images.pexels.com/photos/4534200/pexels-photo-4534200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        image={post.image}
         alt="Paella dish"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          {post.description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
