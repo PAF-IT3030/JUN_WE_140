376b43bdf25395638dd87ec685b7b1ae7262e57d
IT21181160 <99317617+IT21181160@users.noreply.github.com>
2024-04-21 17:23:14 +0530

    follow entity created.

---
+++ backend/strengthHUB/src/main/java/com/bakend/strengthHUB/repo/FollowRepository.java
@@ -0,0 +1,13 @@
+package com.bakend.strengthhub.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.grp_169.application.model.FollowModel;
+
+
+public interface FollowRepository extends JpaRepository <FollowModel, Integer>{
+
+
+
+}
