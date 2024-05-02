8c1242a0b10d6da13ea2d1c75fd6ff2deb53fea4
it21258862 <it21258862@my.sliit.lk>
2024-05-02 17:50:36 +0530

    deleteupdateUpdate

--- frontend/src/Pages/DeleteUpdate.js
+++ frontend/src/Pages/DeleteUpdate.js
@@ -22,25 +22,26 @@ const DeleteUpdate = () => {
         <div className="p-4 mx-10">
           <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] mt-20 p-8 mx-auto">
             <h3 className="text-2xl">
-                Are you sure you want to delete this Workout
+              Are you sure you want to delete this Workout
             </h3>
-
+    
             <div className="flex justify-center mt-8 gap-10">
               <button
                 onClick={handleDeleteWorkout}
                 className="bg-red-600 text-white p-2 w-28 rounded-lg font-semibold"
-            >
-            Yes, Delete
-          </button>
-          <Link to="/updates">
-            <button className="bg-sky-600 text-white p-2 w-28 rounded-lg font-semibold">
-              Cancel
-            </button>
-          </Link>
+              >
+                Yes, Delete
+              </button>
+              <Link to="/updates">
+                <button className="bg-sky-600 text-white p-2 w-28 rounded-lg font-semibold">
+                  Cancel
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-        );
+      );
     };
-
+    
     export default DeleteUpdate;
+    
