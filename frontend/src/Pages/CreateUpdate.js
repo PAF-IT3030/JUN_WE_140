305111d7e45c8f0f660f482be6a7259a78e349dc
it21258862 <it21258862@my.sliit.lk>
2024-05-02 15:56:57 +0530

    createupdate2

--- frontend/src/Pages/CreateUpdate.js
+++ frontend/src/Pages/CreateUpdate.js
@@ -2,3 +2,21 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
+const CreateUpdate = () => {
+    const [exercise, setExercise] = useState("");
+    const [sets, setSets] = useState(0);
+    const [reps, setReps] = useState(0);
+    const [weight, setWeight] = useState(0);
+    const [notes, setNotes] = useState("");
+    const navigate = useNavigate();
+  
+    const handleCreateWorkout = (e) => {
+      const data = {
+        exercise,
+        sets,
+        reps,
+        weight,
+        notes,
+      };
+
+      
