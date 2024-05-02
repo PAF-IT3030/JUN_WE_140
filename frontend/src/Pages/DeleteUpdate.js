fed1c46f44ac1444e5001c228f14d19a08935b05
it21258862 <it21258862@my.sliit.lk>
2024-05-02 17:40:11 +0530

    deleteupdatestart

--- frontend/src/Pages/DeleteUpdate.js
+++ frontend/src/Pages/DeleteUpdate.js
@@ -2,3 +2,8 @@ import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
+const DeleteUpdate = () => {
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+};
