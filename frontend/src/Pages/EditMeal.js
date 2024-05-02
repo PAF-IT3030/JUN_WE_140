ffb14092c64bae7b10f39b034e8cc0bcbd272765
it21258862 <it21258862@my.sliit.lk>
2024-05-02 20:00:59 +0530

    Editmealstart

--- frontend/src/Pages/EditMeal.js
+++ frontend/src/Pages/EditMeal.js
@@ -2,3 +2,13 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
+const EditMeal = () => {
+    const [title, setTitle] = useState("");
+    const [owner, setOwner] = useState(0);
+    const [calories, setCalories] = useState(0);
+    const [plan, setPlan] = useState("");
+    const navigate = useNavigate();
+    const { id } = useParams();
+  
+    
+    };
