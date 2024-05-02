14d9eb101078f8f354706f0009d27478c3e5c7ee
it21258862 <it21258862@my.sliit.lk>
2024-05-02 23:10:41 +0530

    Upadateworkouts

--- frontend/src/Pages/Updates.js
+++ frontend/src/Pages/Updates.js
@@ -2,3 +2,16 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+const Updates = () => {
+    const [workouts, setWorkouts] = useState([]);
+  
+    useEffect(() => {
+      axios
+        .get("http://localhost:8080/student/add")
+        .then((res) => setWorkouts(res.data))
+        .catch((err) => console.log(err));
+    }, []);
+
+};
+
+export default Updates;
