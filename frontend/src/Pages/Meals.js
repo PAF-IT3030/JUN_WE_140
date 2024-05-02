14216f053b49805ecaeff5b8874e849db4fd5a56
it21258862 <it21258862@my.sliit.lk>
2024-05-02 22:33:21 +0530

    Meals1

--- frontend/src/Pages/Meals.js
+++ frontend/src/Pages/Meals.js
@@ -2,3 +2,14 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+const Meals = () => {
+    const [meals, setmeals] = useState([]);
+  
+    useEffect(() => {
+      axios
+        .get("http://localhost:8080/student/add")
+        .then((res) => setMeals(res.data))
+        .catch((err) => console.log(err));
+    }, []);
+};
+export default Meals;
