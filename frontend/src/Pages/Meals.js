a3e28c013f6a5d342b32e4ed0281cc20a477a8c5
it21258862 <it21258862@my.sliit.lk>
2024-05-02 22:37:22 +0530

    MealsAddNewMeal

--- frontend/src/Pages/Meals.js
+++ frontend/src/Pages/Meals.js
@@ -11,5 +11,20 @@ const Meals = () => {
         .then((res) => setMeals(res.data))
         .catch((err) => console.log(err));
     }, []);
+
+    return (
+        <div className="w-full absolute px-20 ">
+          <div className=" mt-24 text-right ">
+            <Link to="/CreateMeal">
+              {" "}
+              <button className=" bg-[#ffb703] px-8 py-1 rounded-lg text-white">
+                Add New Meal
+              </button>
+            </Link>
+          </div>
+          
+        
+        </div>
+      );
 };
 export default Meals;
