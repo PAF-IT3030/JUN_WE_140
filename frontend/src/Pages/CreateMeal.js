fc9ed83d9962a7972e258803d406d98d2c8e08b5
it21258862 <it21258862@my.sliit.lk>
2024-05-06 16:22:03 +0530

    dependencies

--- frontend/src/Pages/CreateMeal.js
+++ frontend/src/Pages/CreateMeal.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 
-const CreateMeal = () => {
+/*const CreateMeal = () => {
     const [title, setTitle] = useState("");
     const [owner, setOwner] = useState();
     const [calories, setCalories] = useState();
@@ -83,6 +83,98 @@ const CreateMeal = () => {
             >
               Share
             </button>
+          </form>
+        </div>
+      </div>
+    );
+  };*/
+
+  const CreateMeal = () => {
+    const [title, setTitle] = useState("");
+    const [owner, setOwner] = useState("");
+    const [calories, setCalories] = useState("");
+    const [plan, setPlan] = useState("");
+    const navigate = useNavigate();
+  
+    const handleCreateMeal = (e) => {
+      e.preventDefault();
+      if (!title || !owner || !calories || !plan) {
+        alert("Please fill in all fields");
+        return;
+      }
+  
+      const data = {
+        title,
+        owner,
+        calories,
+        plan,
+      };
+  
+      axios
+        .post("http://localhost:8080/student/add", data)
+        .then(() => navigate("/meals"))
+        .catch((err) => {
+          alert("Something went wrong...");
+          console.error(err);
+        });
+    };
+  
+    return (
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div className="w-[40%] mx-auto shadow-lg p-5 rounded-lg">
+          <form className="flex flex-col space-y-4">
+            <div>
+              <label className="text-lg font-medium">Title</label>
+              <input
+                type="text"
+                name="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Title"
+                className="block w-full ring-1 ring-slate-400 rounded-md py-2 px-4"
+              />
+            </div>
+            <div>
+              <label className="text-lg font-medium">Owner</label>
+              <input
+                type="text"
+                name="owner"
+                value={owner}
+                onChange={(e) => setOwner(e.target.value)}
+                placeholder="Owner"
+                className="block w-full ring-1 ring-slate-400 rounded-md py-2 px-4"
+              />
+            </div>
+            <div>
+              <label className="text-lg font-medium">Calories</label>
+              <input
+                type="text"
+                name="calories"
+                value={calories}
+                onChange={(e) => setCalories(e.target.value)}
+                placeholder="Calories"
+                className="block w-full ring-1 ring-slate-4 rounded-md py-2 px-400"
+              />
+            </div>
+  
+            <div>
+              <label className="text-lg font-medium">Plan</label>
+              <textarea
+                name="plan"
+                value={plan}
+                onChange={(e) => setPlan(e.target.value)}
+                placeholder="Meal plan"
+                className="block w-full ring-1 ring-slate-400 rounded-md py-2 px-4"
+              />
+            </div>
+            <div className="flex justify-center">
+              <button
+                onClick={handleCreateMeal}
+                className="bg-[#ffb703] px-8 py-2 rounded-lg text-white"
+              >
+                Share
+              </button>
+            </div>
           </form>
         </div>
       </div>
--- frontend/src/Pages/Meals.js
+++ frontend/src/Pages/Meals.js
@@ -1,6 +1,9 @@
+import { Box, Divider, Stack } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import SideBar from "../Components/SideBar";
+import RightSideBar from "../Components/RightSideBar";
 
 const Meals = () => {
     const [meals, setMeals] = useState([]);
@@ -67,50 +70,63 @@ const Meals = () => {
 };*/
 
 return (
-    <div className="w-full absolute px-20 ">
-      <div className=" mt-24 text-right ">
-        <Link to="/CreateMeal">
-          {" "}
-          <button className=" bg-[#ffb703] px-8 py-1 rounded-lg text-white">
-            Add New Meal
-          </button>
-        </Link>
-      </div>
-      <div className="my-10 flex flex-wrap justify-center gap-5 ">
-        {meals.map((meal) => (
-          <div
-            className="w-[300px] h-auto flex flex-col justify-between rounded-lg ring-1 ring-[#ffb703] shadow-lg p-5 text-center "
-            key={meal.id}
-          >
-            <h1 className="text-2xl font-bold mb-5">{meal.title}</h1>
-            <div className="flex justify-center gap-8 items-center mb-5">
-              <h1>
-                <span className="text-lg font-semibold ">Owner: </span>
-                {meal.owner}
-              </h1>
-              <h1>
-                <span className="text-lg font-semibold ">Calories: </span>
-                {meal.calories}
-              </h1>
-            </div>
-            <h1 className=" underline font-semibold ">Plan</h1>
-            <p className="text-justify">{meal.plan}</p>
-            <div className="flex gap-10 justify-center mt-5">
-              <Link to={`/Editupdate/${meal.id}`}>
-                <button className=" bg-[#ffb701] w-24 font-semibold px-4 py-1 rounded-lg text-white">
-                  Edit
-                </button>
-              </Link>
-              <Link to={`/Deleteupdate/${meal.id}`}>
-                <button className=" bg-red-400 w-24 font-semibold px-4 py-1 rounded-lg text-white">
-                  Delete
-                </button>
-              </Link>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+  <>
+  <Box> 
+  <Stack direction="row" justifyContent="space-between">
+ <SideBar/>
+ <Divider orientation="vertical" variant="fullwidth" flexItem />
+ <div className="w-full absolute px-20">
+   <div className=" mt-24 text-right ">
+     <Link to="/CreateMeal">
+       {" "}
+       <button className=" bg-[#ffb703] px-8 py-1 rounded-lg text-white">
+         Add New Meal
+       </button>
+     </Link>
+   </div>
+   <div className="my-10 flex flex-wrap justify-center gap-5 ">
+     {meals.map((meal) => (
+       <div
+         className="w-[300px] h-auto flex flex-col justify-between rounded-lg ring-1 ring-[#ffb703] shadow-lg p-5 text-center "
+         key={meal.id}
+       >
+         <h1 className="text-2xl font-bold mb-5">{meal.title}</h1>
+         <div className="flex justify-center gap-8 items-center mb-5">
+           <h1>
+             <span className="text-lg font-semibold ">Owner: </span>
+             {meal.owner}
+           </h1>
+           <h1>
+             <span className="text-lg font-semibold ">Calories: </span>
+             {meal.calories}
+           </h1>
+         </div>
+         <h1 className=" underline font-semibold ">Plan</h1>
+         <p className="text-justify">{meal.plan}</p>
+         <div className="flex gap-10 justify-center mt-5">
+           <Link to={`/Editupdate/${meal.id}`}>
+             <button className=" bg-[#ffb701] w-24 font-semibold px-4 py-1 rounded-lg text-white">
+               Edit
+             </button>
+           </Link>
+           <Link to={`/Deleteupdate/${meal.id}`}>
+             <button className=" bg-red-400 w-24 font-semibold px-4 py-1 rounded-lg text-white">
+               Delete
+             </button>
+           </Link>
+         </div>
+       </div>
+     ))}
+   </div>
+ </div>
+
+ <Divider orientation="vertical" variant="fullwidth" flexItem />
+ <RightSideBar/>
+</Stack>
+</Box>
+</>
+ 
+   
   );
 };
 export default Meals;
