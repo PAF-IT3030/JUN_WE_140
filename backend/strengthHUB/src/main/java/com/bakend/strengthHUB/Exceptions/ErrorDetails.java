5af9c3258732b3ac68423f8f5c0a279527a58528
chamuda-arangalla <chamudaarangalla@gmail.com>
2024-05-10 18:51:19 +0530

    Add CrossOrigin anotation and create ErrorDetail class.

--- backend/strengthHUB/src/main/java/com/bakend/strengthHUB/Exceptions/ErrorDetails.java
+++ backend/strengthHUB/src/main/java/com/bakend/strengthHUB/Exceptions/ErrorDetails.java
@@ -1,2 +1,19 @@
-package com.bakend.strengthHUB.Exceptions;public class ErrorDetails {
+package com.bakend.strengthHUB.Exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+
+    private String message;
+    private String error;
+    private LocalDateTime timestamp;
+
+
 }
--- backend/strengthHUB/src/main/java/com/bakend/strengthHUB/Exceptions/GlobalExceptions.java
+++ backend/strengthHUB/src/main/java/com/bakend/strengthHUB/Exceptions/GlobalExceptions.java
@@ -1,2 +1,23 @@
-package com.bakend.strengthHUB.Exceptions;public class GlobalExceptions {
+package com.bakend.strengthHUB.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptions {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> otherExceptionHandler(Exception ue,
+                                                              WebRequest req){
+
+        ErrorDetails error = new ErrorDetails(ue.getMessage(),req.getDescription(false), LocalDateTime.now());
+
+        return new ResponseEntity<ErrorDetails>(error, HttpStatus.BAD_REQUEST);
+    }
 }
